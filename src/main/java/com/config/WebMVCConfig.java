6c531d9210874110d36d62118d89572056d11c67
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-11-07 14:09:40 +0700

    07/10

---
+++ src/main/java/com/config/WebMVCConfig.java
@@ -0,0 +1,26 @@
+package com.config;
+
+import com.formatter.ProvinceFormatter;
+import com.service.ProvinceService;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new ProvinceFormatter(applicationContext.getBean(ProvinceService.class)));
+    }
+}
